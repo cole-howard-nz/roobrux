0cfb449d142b5c7d9d55c03e94d987fefe84f1bb
cole <osrunescape.colehoward@gmail.com>
2026-05-12 09:16:51 +1200

    added and ran test cases for caesar_cipher

--- projects/caesar_cipher.rb
+++ projects/caesar_cipher.rb
@@ -20,4 +20,25 @@ def caesar_cipher(user_input, shift_amount)
         .join
 end
 
-puts caesar_cipher("What a string!", 5)
+tests = [
+    { input: ["", 5], expected: "" },
+    { input: ["ABC", 1], expected: "BCD" },
+    { input: ["XYZ", 3], expected: "ABC" },
+    { input: ["abc", 2], expected: "cde" },
+    { input: ["xyz", 2], expected: "zab" },
+    { input: ["Hello, World!", 5], expected: "Mjqqt, Btwqi!" },
+    { input: ["What a string!", 5], expected: "Bmfy f xywnsl!" },
+    { input: ["Shift0", 0], expected: "Shift0" },
+    { input: ["b", -1], expected: "a" },
+    { input: ["a", -1], expected: "z" },
+    { input: ["A", -1], expected: "Z" },
+    { input: ["abc", 26], expected: "abc" },
+    { input: ["ABC", 52], expected: "ABC" },
+    { input: ["Rotate by 13", 13], expected: "Ebgngr ol 13" },
+    { input: ["12345!@#", 7], expected: "12345!@#" }
+]
+
+tests.each { |test| 
+    result = caesar_cipher(test[:input][0], test[:input][1])
+    puts "Original-> #{ test[:input][0] }\nResult->   #{ result }\n#{ result.eql?(test[:expected]) ? "PASSED" : "FAILED" }\n\n"
+}
